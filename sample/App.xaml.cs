344590272f18ccb297436eb092df25846a278798
Alex <33064621+Axemasta@users.noreply.github.com>
2025-01-21 22:16:00 +0000

    Fixed Android Crash
    The bottom sheet no longer crashes when presenting

--- sample/App.xaml.cs
+++ sample/App.xaml.cs
@@ -1,11 +1,12 @@
-﻿namespace The49.Maui.BottomSheet.Sample;
+﻿
+namespace The49.Maui.BottomSheet.Sample;
 
 public partial class App : Application
 {
     public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState? activationState) => new(new AppShell());
 }
